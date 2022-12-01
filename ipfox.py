bbabb13c036a86fdc1a710f3f8d97aba93e3f2cc
spencerwooo <spencer.woo@outlook.com>
2022-12-01 08:46:09 +0000

    update mail send process

--- ipfox.py
+++ ipfox.py
@@ -1,68 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import argparse
+import configparser
+import logging
 import smtplib
 import socket
+from datetime import datetime
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from time import asctime
-import argparse
-import configparser
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-c','--path', 
-						type=str, 
-						default='ipfox.ini', 
-						metavar='STRING',
-						help='init'
-					)
-args = parser.parse_args()
-path = args.path
+class IPFox:
+    def __init__(self, path):
+        logging.info(f"Reading config file: {path}")
+        config = configparser.ConfigParser()
+        config.read(path)
 
-config = configparser.ConfigParser()
-config.read(path) 
-RemoteIP = config['Connection']['RemoteIP']
-RemotePort = int(config['Connection']['RemotePort'])
-Hostname = config['Email']['Hostname']
-SenderHost = config['Email']['SenderHost']
-SenderUser = config['Email']['SenderUser']
-SenderAuth = config['Email']['SenderAuth']
-ReceiverList1 = config['Email']['ReceiverList1']
-ReceiverList2 = config['Email']['ReceiverList2']
+        self.remote_ip = config["Connection"]["RemoteIP"]
+        self.remote_port = int(config["Connection"]["RemotePort"])
 
+        self.hostname = config["Email"]["Hostname"]
+        self.subject = config["Email"]["Subject"]
 
-def send_an_email(email_content):
-    mail_host = SenderHost
-    mail_user = SenderUser
-    mail_auth_code = SenderAuth
-    mail_sender = mail_user  
-    mail_receivers = [ReceiverList1,ReceiverList2]
-    message = MIMEMultipart()
-    message["From"] = Header(mail_sender)
-    message["Subject"] = Header(f"{Hostname} IP Address")
-    message.attach(MIMEText(asctime(), "plain", "utf-8"))
-    message.attach(MIMEText(email_content, "plain", "utf-8"))
-    print("message is {}".format(message.as_string()))
-    smtpObj = smtplib.SMTP(mail_host)
-    # smtpObj.set_debuglevel(1)
-    smtpObj.login(mail_user, mail_auth_code)
-    smtpObj.sendmail(mail_sender, mail_receivers, message.as_string())
+        self.sender_host = config["Email"]["SenderHost"]
+        self.sender_user = config["Email"]["SenderUser"]
+        self.sender_auth = config["Email"]["SenderAuth"]
+        self.receivers = [mail for _, mail in config.items("ReceiverList")]
 
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((self.remote_ip, self.remote_port))
+        ip = s.getsockname()[0]
+        logging.info(f"Current host IP: {ip}")
+        return ip
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect((RemoteIP, RemotePort))
-    res = s.getsockname()[0]
-    print(res)
-    send_ip = res
-    whether_to_send = True
-    return whether_to_send, send_ip
+    def send_mail(self):
+        # Get current time
+        def now():
+            return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        content = f"{self.hostname} rebooted at UTC {now()}, ip: {self.get_ip()}"
+        logging.info(f"Email content: {content}")
+
+        message = MIMEMultipart()
+        message["From"] = Header(self.sender_user)
+        message["To"] = Header(",".join(self.receivers))
+        message["Subject"] = Header(self.subject)
+
+        message.attach(MIMEText(content, "plain", "utf-8"))
+
+        logging.info("Sending email...")
+        with smtplib.SMTP(self.sender_host) as smtp:
+            smtp.login(self.sender_user, self.sender_auth)
+            smtp.sendmail(self.sender_user, self.receivers, message.as_string())
+            logging.info(f"Done, email sent at time {now()}.")
 
 
 if __name__ == "__main__":
-    whether_to_send, global_ips = get_ip()
-    if whether_to_send:
-        send_an_email(global_ips)
-    else:
-        print("wait and no send")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default="ipfox.ini",
+        metavar="STRING",
+        help="path to config file",
+    )
+    args = parser.parse_args()
+    path = args.config
+
+    ipfox = IPFox(path)
+    ipfox.send_mail()
