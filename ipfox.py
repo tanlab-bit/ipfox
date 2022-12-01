d627a85f14b67ce8b4324a4a82924f0a5f81d8ee
BIT-QiuYu <pengrongq@163.com>
2022-12-01 07:22:49 +0000

    add argparse/configparse

--- ipfox.py
+++ ipfox.py
@@ -6,33 +6,56 @@ from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from time import asctime
+import argparse
+import configparser
 
 
-def send_an_email(email_content):  # email_content是一个字符串
-    mail_host = "smtp.163.com"  # 这个去邮箱找
-    mail_user = "bitqiuyu@163.com"
-    mail_auth_code = "YCQVNMUZPOLQETIM"  # 授权码
-    mail_sender = mail_user  # 用mail_user 作为发送人
-    mail_receivers = ["pengrongq@163.com"]
+parser = argparse.ArgumentParser()
+parser.add_argument('-c','--path', 
+						type=str, 
+						default='ipfox.ini', 
+						metavar='STRING',
+						help='init'
+					)
+args = parser.parse_args()
+path = args.path
+
+config = configparser.ConfigParser()
+config.read(path) 
+RemoteIP = config['Connection']['RemoteIP']
+RemotePort = int(config['Connection']['RemotePort'])
+Hostname = config['Email']['Hostname']
+SenderHost = config['Email']['SenderHost']
+SenderUser = config['Email']['SenderUser']
+SenderAuth = config['Email']['SenderAuth']
+ReceiverList1 = config['Email']['ReceiverList1']
+ReceiverList2 = config['Email']['ReceiverList2']
+
+def send_an_email(email_content):
+    mail_host = SenderHost
+    mail_user = SenderUser
+    mail_auth_code = SenderAuth
+    mail_sender = mail_user  
+    mail_receivers = [ReceiverList1,ReceiverList2]
     message = MIMEMultipart()
-    message["From"] = Header(mail_sender)  # 寄件人
-    message["Subject"] = Header("cs928-1080本机ip地址")
+    message["From"] = Header(mail_sender)
+    message["Subject"] = Header(f"{Hostname} IP Address")
     message.attach(MIMEText(asctime(), "plain", "utf-8"))
     message.attach(MIMEText(email_content, "plain", "utf-8"))
-    print("message is {}".format(message.as_string()))  # debug用
+    print("message is {}".format(message.as_string()))
     smtpObj = smtplib.SMTP(mail_host)
-    # smtpObj.set_debuglevel(1) # 同样是debug用的
-    smtpObj.login(mail_user, mail_auth_code)  # 登陆
-    smtpObj.sendmail(mail_sender, mail_receivers, message.as_string())  # 真正发送邮件就是这里
+    # smtpObj.set_debuglevel(1)
+    smtpObj.login(mail_user, mail_auth_code)
+    smtpObj.sendmail(mail_sender, mail_receivers, message.as_string())
 
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("10.0.0.55", 80))
+    s.connect((RemoteIP, RemotePort))
     res = s.getsockname()[0]
     print(res)
     send_ip = res
-    whether_to_send = False
+    whether_to_send = True
     return whether_to_send, send_ip
 
 
